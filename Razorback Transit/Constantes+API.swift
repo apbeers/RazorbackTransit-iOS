348e045c36bba37883904ba568528c347bdb40b3
Andrew Beers <apbeers@gmail.com>
2017-11-07 09:55:41 -0600

    fix bus api

--- Razorback Transit/Constantes+API.swift
+++ Razorback Transit/Constantes+API.swift
@@ -14,6 +14,7 @@ extension Constants {
         static let RouteURL = "http://campusdata.uark.edu/api/routes?callback=Routes"
         //static let StopURL = "https://campusdata.uark.edu/api/stops?callback=Stopsnull&routeIds=undefined-221-223-224-226-227-228-229-231-284"
         static let StopURL = "https://campusdata.uark.edu/api/stops?callback=Stopsnull&routeIds=undefined"
-        static let BusURL = "https://campusdata.uark.edu/signalr?transport=longPolling&connectionId=79fa1891-1a55-499b-8e75-68a7c680d5df&connectionData=%5B%7B%22name%22%3A%22BusesHub%22%7D%5D&messageId=5580&tid=7"
+        static let BusURL = "https://campusdata.uark.edu/api/buses?callback=jQuery18002674589609856972_1510069338014&routeIds=undefined-221-223-224-226-227-228-229-231-284&_=1510069339459"
+        //static let BusURL = "https://campusdata.uark.edu/signalr?transport=longPolling&connectionId=79fa1891-1a55-499b-8e75-68a7c680d5df&connectionData=%5B%7B%22name%22%3A%22BusesHub%22%7D%5D&messageId=5580&tid=7"
     }
 }
